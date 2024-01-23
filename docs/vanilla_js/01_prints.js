411ef29e4fb0a1f59befb9c133d6b74b41611d58
DeokJae Kim <kdj0712@gmail.com>
2024-01-23 12:29:21 +0900

    first commit

---
+++ docs/vanilla_js/01_prints.js
@@ -0,0 +1,27 @@
+let Name = "DeokJaeKim";
+// undefined
+console.log(Name)
+// VM243:1 DeokJaeKim
+// undefined
+let welcome = 'Hello World!'
+// undefined
+welcome
+// 'Hello World!'
+let numbers = 55
+// undefined
+numbers
+// 55
+let concat = 'String:'+welcome+',Number:'+numbers
+// undefined
+concat
+// 'String:Hello World!,Number:55'
+alert(concat)
+// undefined
+`String : ${welcome}, Number : ${numbers}`
+// 'String : Hello World!, Number : 55'
+let concat_tilt = `String : ${welcome}, Number : ${numbers}`
+// undefined
+concat_tilt
+// 'String : Hello World!, Number : 55'
+console.log(concat_tilt)
+// VM1442:1 String : Hello World!, Number : 55
