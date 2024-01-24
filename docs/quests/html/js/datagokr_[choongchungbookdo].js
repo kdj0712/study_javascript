3a9a5040ad6bce6e447236efc0eb2198d66089c3
DeokJae Kim <kdj0712@gmail.com>
2024-01-24 20:14:16 +0900

    make paginations

--- docs/quests/html/js/datagokr_[choongchungbookdo].js
+++ docs/quests/html/js/datagokr_[choongchungbookdo].js
@@ -1,16 +1,14 @@
-let shopping_insite = document.querySelector("#check");
-shopping_insite.addEventListener('click' , async (event) => {
-    let url ='http://apis.data.go.kr/6430000/bioDistCompanyStatService/getbioDistCompanyStat?serviceKey=yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr%2BR%2Bw1LLReXys9t18Q27yIA%3D%3D&currentPage=1&perPage=10';
-    // let option = {};
-    // let body = {
-    //     "serviceKey":"yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr%2BR%2Bw1LLReXys9t18Q27yIA%3D%3D&"
-    //     ,"currentPage":"1"
-    //     ,"perPage":"5"
-    //   };
-    // let option = {
-    //     method : "GET"
-    //     ,Body : JSON.stringify(body)
-    // };
+let currentPage = 1;
+// 이전 페이지 버튼 이벤트 리스너
+
+async function fetchPage(pageNumber) {
+    let url ='http://apis.data.go.kr/6430000/bioDistCompanyStatService/getbioDistCompanyStat?';
+    let Params = new URLSearchParams({
+        serviceKey: "yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr%2BR%2Bw1LLReXys9t18Q27yIA%3D%3D",
+        currentPage: currentPage.toString(),  // 현재 페이지 번호
+        perPage: "5"  // 한 페이지에 보여줄 아이템의 수
+    });
+    url = url + Params.toString();
     try {
         let response = await fetch(url); // send
         let result = await response.json();       // response
@@ -22,7 +20,45 @@ shopping_insite.addEventListener('click' , async (event) => {
             }
             let animals_elements = document.querySelector("#animals_arrays_id");
             animals_elements.innerHTML = animals_list;
+            document.querySelector("#currentPage").innerText = currentPage.toString();
     } catch (error) {
         console.log(`Error message : ${error.message}`)
     }
+};
+
+// 조회하기 버튼 이벤트 리스너
+document.querySelector("#check").addEventListener('click', () => {
+    currentPage = 1;
+    fetchPage(currentPage);
 });
+
+// 이전 페이지 버튼 이벤트 리스너
+document.querySelector("#prevPage").addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        fetchPage(currentPage);
+    } else {
+        alert('첫 페이지입니다.');
+    }
+});
+
+// 다음 페이지 버튼 이벤트 리스너
+document.querySelector("#nextPage").addEventListener('click', async () => {
+    let nextUrl ='http://apis.data.go.kr/6430000/bioDistCompanyStatService/getbioDistCompanyStat?';
+    let nextParams = new URLSearchParams({
+        serviceKey: "yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr%2BR%2Bw1LLReXys9t18Q27yIA%3D%3D",
+        currentPage: (currentPage + 1).toString(),  // 다음 페이지 번호
+        perPage: "5"  // 한 페이지에 보여줄 아이템의 수
+    });
+
+    nextUrl = nextUrl + nextParams.toString();
+    let nextResponse = await fetch(nextUrl);
+    let nextResult = await nextResponse.json();
+    if (nextResult['body'].length > 0) {
+        currentPage++;
+        fetchPage(currentPage);
+    } else {
+        alert('다음 페이지가 없습니다.');
+    }
+});
+fetchPage(1) 
