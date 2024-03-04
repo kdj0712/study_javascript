9e7c3c2546663b16069892332bd68f5e17e920d9
DeokJae Kim <kdj0712@gmail.com>
2024-03-04 18:54:30 +0900

    commit

--- docs/quests/html/js/datagokr_[choongchungbookdo].js
+++ docs/quests/html/js/datagokr_[choongchungbookdo].js
@@ -2,37 +2,38 @@ let currentPage = 1;
 // 이전 페이지 버튼 이벤트 리스너
 
 async function fetchPage(pageNumber) {
-    let url ='http://apis.data.go.kr/6430000/bioDistCompanyStatService/getbioDistCompanyStat?';
+    let url ='http://apis.data.go.kr/6430000/bioDistCompanyStatService/getbioDistCompanyStat';
     let Params = new URLSearchParams({
-        serviceKey: "yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr%2BR%2Bw1LLReXys9t18Q27yIA%3D%3D",
+        serviceKey: "yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr+R+w1LLReXys9t18Q27yIA==",
         currentPage: currentPage.toString(),  // 현재 페이지 번호
         perPage: "5"  // 한 페이지에 보여줄 아이템의 수
     });
-    url = url + Params.toString();
+    url = url + "?" + Params;
     try {
         let response = await fetch(url); // send
         let result = await response.json();       // response
         console.log(`response result : ${result}`) // print
+        // JSON.parse(result);
         const results = result['body'];
-        let animals_list = "";
-            for (let animal_object of results) {
-            animals_list = `${animals_list}<tr><td>${animal_object["FCLTY_NM"]}</td><td>${animal_object["MAIN_SKLL"]}</td><td>${animal_object["PRTN_SITTN"]}</td></tr>`;
+        let comapany_list = "";
+            for (let comapany_object of results) {
+            comapany_list = `${comapany_list}<tr><td>${comapany_object["FCLTY_NM"]}</td><td>${comapany_object["MAIN_SKLL"]}</td><td>${comapany_object["PRTN_SITTN"]}</td></tr>`;
             }
-            let animals_elements = document.querySelector("#animals_arrays_id");
-            animals_elements.innerHTML = animals_list;
+            let comapany_elements = document.querySelector("#company_arrays_id");
+            comapany_elements.innerHTML = comapany_list;
             document.querySelector("#currentPage").innerText = currentPage.toString();
     } catch (error) {
         console.log(`Error message : ${error.message}`)
     }
 };
 
-// 조회하기 버튼 이벤트 리스너
+// 조회하기 버튼 이벤트
 document.querySelector("#check").addEventListener('click', () => {
     currentPage = 1;
     fetchPage(currentPage);
 });
 
-// 이전 페이지 버튼 이벤트 리스너
+// 이전 페이지 버튼 이벤트
 document.querySelector("#prevPage").addEventListener('click', () => {
     if (currentPage > 1) {
         currentPage--;
@@ -42,16 +43,16 @@ document.querySelector("#prevPage").addEventListener('click', () => {
     }
 });
 
-// 다음 페이지 버튼 이벤트 리스너
+// 다음 페이지 버튼 이벤트 
 document.querySelector("#nextPage").addEventListener('click', async () => {
-    let nextUrl ='http://apis.data.go.kr/6430000/bioDistCompanyStatService/getbioDistCompanyStat?';
+    let nextUrl ='http://apis.data.go.kr/6430000/bioDistCompanyStatService/getbioDistCompanyStat';
     let nextParams = new URLSearchParams({
-        serviceKey: "yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr%2BR%2Bw1LLReXys9t18Q27yIA%3D%3D",
+        serviceKey: "yJgnzrDPngw7nw8rHXdq2exc9xxcXlYp9knUeoaMTvMiiQipAFQsyhr9xYT2a0sr+R+w1LLReXys9t18Q27yIA==",
         currentPage: (currentPage + 1).toString(),  // 다음 페이지 번호
         perPage: "5"  // 한 페이지에 보여줄 아이템의 수
     });
 
-    nextUrl = nextUrl + nextParams.toString();
+    nextUrl = nextUrl + "?" + nextParams;
     let nextResponse = await fetch(nextUrl);
     let nextResult = await nextResponse.json();
     if (nextResult['body'].length > 0) {
